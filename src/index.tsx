1846cda9d724b3775cdb81f9103b738f85e380d4
toshiakifukikoshi <86909359+toshiakifukikoshi@users.noreply.github.com>
2022-05-06 17:54:58 +0900

    現在デフォルトのスタイル完成

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,34 @@
+// import { StrictMode } from "react";
+// import * as ReactDOMClient from "react-dom/client";
+
+// import App from "./App";
+
+// const rootElement = document.getElementById("root");
+// const root = ReactDOMClient.createRoot(rootElement);
+
+// root.render(
+//   <StrictMode>
+//     <App />
+//   </StrictMode>
+// );
+
+// import { createRoot } from "react-dom/client";
+
+// import App from "./App";
+
+// const rootElement = document.getElementById("root");
+// const root = createRoot(rootElement);
+
+// root.render(<App />);
+
 import { StrictMode } from "react";
-import * as ReactDOMClient from "react-dom/client";
+import ReactDom from "react-dom";
 
 import App from "./App";
 
-const rootElement = document.getElementById("root");
-const root = ReactDOMClient.createRoot(rootElement);
-
-root.render(
+ReactDom.render(
   <StrictMode>
     <App />
-  </StrictMode>
+  </StrictMode>,
+  document.getElementById("root")
 );
