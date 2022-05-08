9e7fbf76328c67b175e1b5996bf73dcdbfb654f8
toshiakifukikoshi <86909359+toshiakifukikoshi@users.noreply.github.com>
2022-05-08 23:37:39 +0900

    うまくいった

--- src/components/molecules/MenuDrawer.tsx
+++ src/components/molecules/MenuDrawer.tsx
@@ -11,10 +11,19 @@ import {
 type Props = {
   onClose: () => void;
   isOpen: boolean;
+  onClickHome: () => void;
+  onClickUserManagement: () => void;
+  onClickSetting: () => void;
 };
 
 export const MenuDrawer: VFC<Props> = memo((props) => {
-  const { onClose, isOpen } = props;
+  const {
+    onClose,
+    isOpen,
+    onClickHome,
+    onClickUserManagement,
+    onClickSetting
+  } = props;
 
   return (
     <>
@@ -22,9 +31,15 @@ export const MenuDrawer: VFC<Props> = memo((props) => {
         <DrawerOverlay>
           <DrawerContent>
             <DrawerBody p={0} bg="gray.100">
-              <Button w="100%">TOP</Button>
-              <Button w="100%">ユーザー一覧</Button>
-              <Button w="100%">設定</Button>
+              <Button w="100%" onClickHome={onClickHome}>
+                TOP
+              </Button>
+              <Button w="100%" onClickUserMangement={onClickUserManagement}>
+                ユーザー一覧
+              </Button>
+              <Button w="100%" onClickSetting={onClickSetting}>
+                設定
+              </Button>
             </DrawerBody>
           </DrawerContent>
         </DrawerOverlay>
