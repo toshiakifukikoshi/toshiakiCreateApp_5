f6283ac0552dd3643e6dd043336c3afacfcb034d
toshiakifukikoshi <86909359+toshiakifukikoshi@users.noreply.github.com>
2022-05-11 23:46:46 +0900

    俊明酒飲んだ

--- src/components/pages/UserManagement.tsx
+++ src/components/pages/UserManagement.tsx
@@ -1,9 +1,22 @@
 import { VFC, memo } from "react";
+import { Box, Wrap, WrapItem, Stack, Image } from "@chakra-ui/react";
 
 export const UserManagement: VFC = memo(() => {
   return (
     <>
-      <p>ユーザー管理画面になります</p>
+      <Wrap>
+        <WrapItem>
+          <Box w="260px" h="260px" bg="white" borderRadius="10px" shadow="md">
+            <Stack textalign="center">
+              <Image
+                borderRadius="full"
+                boxSize="160px"
+                src="https://source.unsplash.com/random"
+              />
+            </Stack>
+          </Box>
+        </WrapItem>
+      </Wrap>
     </>
   );
 });
--- src/hooks/useAuth.ts
+++ src/hooks/useAuth.ts
@@ -3,9 +3,11 @@ import { useCallback, useState } from "react";
 import { useHistory } from "react-router-dom";
 
 import { User } from "../types/api/user";
+import { useMessage } from "./useMessage";
 
 export const useAuth = () => {
   const history = useHistory();
+  const { showMessage } = useMessage();
 
   const [loading, setLoading] = useState(false);
 
@@ -16,19 +18,24 @@ export const useAuth = () => {
         .get<Array<User>>(`https://jsonplaceholder.typicode.com/users/${id}`)
         .then((res) => {
           if (res.data) {
+            showMessage({
+              title: "ログインしました",
+              status: "success"
+            });
             history.push("/home");
           } else {
+            showMessage({ title: "ユーザーが見つかりません", status: "error" });
             alert("ユーザーが見つかりません");
           }
         })
         .catch(() => {
-          alert("ログインできません");
+          showMessage({ title: "ログインできません　", status: "warning" });
         })
         .finally(() => {
           setLoading(false);
         });
     },
-    [history]
+    [history, showMessage]
   );
   return { login, loading };
 };
